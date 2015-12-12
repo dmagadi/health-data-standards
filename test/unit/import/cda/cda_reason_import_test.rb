086114525f9fadee932c5e1042af36752e1edcad
pophealth <pophealth@ubuntu.(none)>
2015-12-12 15:16:39 -0800

    Fix for BMI measure which needs medication reason to be counted in numerator. This fix just parses the medication reason and attaches the reason to the object

---
+++ test/unit/import/cda/cda_reason_import_test.rb
@@ -0,0 +1,49 @@
+	require 'test_helper'
+	module CCDA
+		class CDAMedicationReasonTest < Minitest::Test
+			def setup
+				@doc = Nokogiri::XML(File.new('test/fixtures/cda_reasons/1_patient.xml'))
+				@doc.root.add_namespace_definition('cda', 'urn:hl7-org:v3')
+				@doc.root.add_namespace_definition('sdtc', 'urn:hl7-org:sdtc')
+				@db = Mongoid.default_session
+				dump_database
+				assert_clean_db
+			end
+			def assert_clean_db
+				["records","measures","bundles","patient_cache","query_cache"].each do |collection|
+					assert_equal 0, @db[collection].where({}).count , "Should be 0 #{collection} in the db"
+				end
+			end
+		def test_parse_ccda
+			#	patient = HealthDataStandards::Import::Cat1::PatientImporter.instance.parse_cat1 (@doc)
+			# assert_equal 3, patient.allergies.size
+			# allergy = patient.allergies.first
+			#assert_equal "247472004", allergy.reaction['code']
+			#assert_equal "371924009", allergy.severity['code']
+			#assert_equal "416098002", allergy.type['code']
+			#assert_equal "active", allergy.status
+			#condition = patient.conditions.first
+			#efute_nil condition
+			#assert_equal "Complaint", condition.type
+			#assert_equal 0, patient.encounters.size
+			#assert_equal 4, patient.immunizations.size
+			#medication = patient.medications.first
+			#refute_nil medication
+			#assert_equal "5955009", medication.vehicle["code"]
+			#refute_nil medication.order_information.first
+			#refute_nil medication.fulfillment_history.first
+			#assert_equal "248342006", medication.reason["code"]
+			#record = Record.update_or_create(patient)
+			#assert_equal 3, patient.procedures.size
+		end
+ 		def test_bulk_record_import_xml_archive
+		    file = File.new('test/fixtures/bulk_importer/patients.zip')
+		    HealthDataStandards::Import::BulkRecordImporter.import_archive(file,"./tmp/failed")
+		    #patient1 = Record.where({"first" => "Cancer_Adult_Female"}).first
+		    #patient2 = Record.where({"first" => "Cancer_Adult_Male"}).first
+		    #assert_equal patient1["encounters"].count, 2, "Patient1 should have 2 encounters"
+		    #assert_equal patient2["encounters"].count, 2, "Patient2 should have 2 encounters"
+  		end
+
+	end
+end
